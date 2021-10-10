c39d61560089292d50758b8144eb426ed0ba71f7
hdsdi3g <admin@hd3g.tv>
2021-10-10 22:59:53 +0200

    Create an Hello world base project for #1. Add CSS (local) font.

---
+++ src/main/java/media/mexm/weblivegraphics/App.java
@@ -0,0 +1,29 @@
+/*
+ * This file is part of weblivegraphics.
+ *
+ * weblivegraphics is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * weblivegraphics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * Copyright (C) Media ex Machina 2021
+ *
+ */
+package media.mexm.weblivegraphics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
